faf9eb619a782388331fbdd588dadbe18d0f1408
Simon Szustkowski <mail@simonszu.de>
2018-07-05 12:46:38 +0200

    First collection

---
+++ fish_colors.fish
@@ -0,0 +1,8 @@
+#! /usr/bin/fish
+for i in (seq 0 255)
+  printf "\x1b[38;5;"$i"mcolor%03d\x1b[0m " $i
+  if test (math \($i+1\)%8) -eq 0
+    echo
+  end
+end
+echo "Use color numbers without leading zeros!"
